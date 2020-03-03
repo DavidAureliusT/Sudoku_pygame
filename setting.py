c082498a3e12ca5ac3d00dcaf8e51ee0c1bf1288
daurelius <55678808+daurelius@users.noreply.github.com>
2020-03-03 09:37:47 +0700

    draws selected box

--- setting.py
+++ setting.py
@@ -4,10 +4,12 @@ HEIGHT = 600
 # Colours
 WHITE = (255,255,255)
 BLACK = (0,0,0)
+LIGHTBLUE = (96,126, 232)
 
 # Boards
 testBoard = [[0 for x in range(9)] for x in range(9)]
 
 # Positions and sizes
 gridPos = (75,100)
-cellsSize = 50
+cellSize = 50
+gridSize = cellSize *9
